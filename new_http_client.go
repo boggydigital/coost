949739f39dd7660aec154128b2d00307549fd715
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-01-27 18:29:44 -0800

    Adding convenience method to create httpClient from a file path and hosts

--- new_http_client.go
+++ new_http_client.go
@@ -3,6 +3,7 @@ package coost
 import (
 	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -22,4 +23,29 @@ func (pj persistentJar) NewHttpClient() *http.Client {
 		},
 		Jar: pj,
 	}
+}
+
+func NewHttpClientFromFile(path string, hosts ...string) (*http.Client, error) {
+
+	if _, err := os.Stat(path); err != nil {
+		if !os.IsNotExist(err) {
+			return nil, err
+		} else {
+			pj := &persistentJar{}
+			return pj.NewHttpClient(), nil
+		}
+	}
+
+	cookieFile, err := os.Open(path)
+	defer cookieFile.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	pj, err := NewJar(cookieFile, hosts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return pj.NewHttpClient(), nil
 }
